717cd6974ac0b83faad2bd48584fafad3053bc2a
Nick <nlogan@gmail.com>
2014-10-13 12:54:06 -0400

    dev version increment

--- lib/Text/Levenshtein/XS.pm
+++ lib/Text/Levenshtein/XS.pm
@@ -5,7 +5,7 @@ use warnings FATAL => 'all';
 require Exporter;
 
 @Text::Levenshtein::XS::ISA       = qw/Exporter/;
-$Text::Levenshtein::XS::VERSION   = qw/0.42/;
+$Text::Levenshtein::XS::VERSION   = qw/0.42_01/;
 @Text::Levenshtein::XS::EXPORT_OK = qw/distance/;
 
 eval {
