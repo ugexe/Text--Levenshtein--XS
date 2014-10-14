6899062726b3e52bd222905027f4a91c98c5a2be
Nick <nlogan@gmail.com>
2014-10-14 01:42:23 -0400

    Build of release v0.425 (40e2e7a on dev)

--- lib/Text/Levenshtein/XS.pm
+++ lib/Text/Levenshtein/XS.pm
@@ -7,7 +7,7 @@
 # the same terms as the Perl 5 programming language system itself.
 #
 package Text::Levenshtein::XS;
-$Text::Levenshtein::XS::VERSION = '0.424'; # TRIAL
+$Text::Levenshtein::XS::VERSION = '0.425';
 use 5.008;
 use strict;
 use warnings FATAL => 'all';
@@ -38,7 +38,7 @@ Text::Levenshtein::XS - Calculate edit distance based on insertion, deletion, su
 
 =head1 VERSION
 
-version 0.424
+version 0.425
 
 =head1 SYNOPSIS
 
